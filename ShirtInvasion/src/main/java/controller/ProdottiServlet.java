e77e0ac5850e4397e5df8a1520c5809c25cc1dbe
Nicola Zimbardi <n.zimbardi1@studenti.unisa.it>
2026-06-19 18:13:56 +0200

    modifica classe prodottiservlet

--- ShirtInvasion/src/main/java/controller/ProdottiServlet.java
+++ ShirtInvasion/src/main/java/controller/ProdottiServlet.java
@@ -1,41 +1,35 @@
 package controller;
 
+import java.io.IOException;
+import java.util.List;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import dao.ProdottoDAO;
+import model.Prodotto;
 
-/**
- * Servlet implementation class ProdottiServlet
- */
 @WebServlet("/ProdottiServlet")
 public class ProdottiServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ProdottiServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+        ProdottoDAO prodottoDao = new ProdottoDAO();
+        String idParam = request.getParameter("id");
+        
+        if (idParam != null) {
+            int id = Integer.parseInt(idParam);
+            Prodotto prodotto = prodottoDao.doRetrieveById(id);
+            request.setAttribute("prodotto", prodotto);
+            request.getRequestDispatcher("/WEB-INF/views/dettaglio.jsp").forward(request, response);
+        } else {
+            
+            List<Prodotto> lista = prodottoDao.doRetrieveAll();
+            request.setAttribute("prodotti", lista);
+            request.getRequestDispatcher("/WEB-INF/views/prodotti.jsp").forward(request, response);
+        }
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
