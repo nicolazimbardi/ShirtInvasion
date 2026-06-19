2facce174b988039b9b6fc46cbc520b31646d37d
Nicola Zimbardi <n.zimbardi1@studenti.unisa.it>
2026-06-19 10:58:03 +0200

    creazione carrello.java

--- ShirtInvasion/src/main/java/model/Carrello.java
+++ ShirtInvasion/src/main/java/model/Carrello.java
@@ -1,5 +1,39 @@
 package model;
 
-public class Carrello {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Carrello implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private List<Prodotto> elementi;
+
+    public Carrello() {
+        this.elementi = new ArrayList<>();
+    }
+
+    public List<Prodotto> getElementi() {
+        return elementi;
+    }
+
+    public void aggiungiProdotto(Prodotto p) {
+        this.elementi.add(p);
+    }
+
+    public void rimuoviProdotto(int idProdotto) {
+        this.elementi.removeIf(p -> p.getIdProdotto() == idProdotto);
+    }
+
+    public double getPrezzoTotale() {
+        double totale = 0;
+        for (Prodotto p : elementi) {
+            totale += p.getPrezzo();
+        }
+        return totale;
+    }
+
+    public void svuota() {
+        this.elementi.clear();
+    }
 }
