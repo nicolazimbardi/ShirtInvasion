4fb5a834c7b8e6ebe3b61fd115a94600782d3428
Nicola Zimbardi <n.zimbardi1@studenti.unisa.it>
2026-06-19 10:34:20 +0200

    implementazione utente,java

--- ShirtInvasion/src/main/java/model/Utente.java
+++ ShirtInvasion/src/main/java/model/Utente.java
@@ -1,5 +1,59 @@
 package model;
 
-public class Utente {
+import java.io.Serializable;
 
+public class Utente implements Serializable {
+
+    private int idUtente;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String password;
+    private String indirizzo;
+    private String telefono;
+    private String ruolo; 
+
+    
+    public Utente() {}
+
+    public Utente(int idUtente, String nome, String cognome, String email, String password, String indirizzo, String telefono, String ruolo) {
+        this.idUtente = idUtente;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.password = password;
+        this.indirizzo = indirizzo;
+        this.telefono = telefono;
+        this.ruolo = ruolo;
+    }
+
+    
+    public int getIdUtente() { return idUtente; }
+    public void setIdUtente(int idUtente) { this.idUtente = idUtente; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCognome() { return cognome; }
+    public void setCognome(String cognome) { this.cognome = cognome; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getIndirizzo() { return indirizzo; }
+    public void setIndirizzo(String indirizzo) { this.indirizzo = indirizzo; }
+
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+
+    public String getRuolo() { return ruolo; }
+    public void setRuolo(String ruolo) { this.ruolo = ruolo; }
+
+   
+    public String toString() {
+        return "Utente [idUtente=" + idUtente + ", nome=" + nome + ", cognome=" + cognome + ", email=" + email + ", ruolo=" + ruolo + "]";
+    }
 }
