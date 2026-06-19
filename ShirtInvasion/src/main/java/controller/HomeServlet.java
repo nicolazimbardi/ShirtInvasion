a9f3b4b6368adb1a1ba07ad6b6f8f694eefb3ce8
Lorenzo Scariati <scariatilorenzo7@gmail.com>
2026-06-19 17:38:52 +0200

    Correzione codice HomeServlet

--- ShirtInvasion/src/main/java/controller/HomeServlet.java
+++ ShirtInvasion/src/main/java/controller/HomeServlet.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import dao.ProdottoDAO;
 import model.Prodotto;
 
-@WebServlet("") 
+@WebServlet("/home") 
 public class HomeServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
