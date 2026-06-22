30c85f5a713eebfd1ff46da0469f5596aad902dd
Nicola Zimbardi <n.zimbardi1@studenti.unisa.it>
2026-06-22 16:13:51 +0200

    modifica errori tasto torna alla home

--- ShirtInvasion/src/main/WebContent/scripts/script.js
+++ ShirtInvasion/src/main/WebContent/scripts/script.js
@@ -19,3 +19,22 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+// Gestione  dello scroll al click di aggiungi al carrello
+document.addEventListener('DOMContentLoaded', function() {
+    const pulsantiCarrello = document.querySelectorAll('.btn-add-cart');
+    
+    if (pulsantiCarrello.length > 0) {
+        pulsantiCarrello.forEach(btn => {
+            btn.addEventListener('click', () => {
+                localStorage.setItem('catalogoScrollPos', window.scrollY);
+            });
+        });
+
+        const scrollPos = localStorage.getItem('catalogoScrollPos');
+        if (scrollPos) {
+            window.scrollTo(0, parseInt(scrollPos));
+            localStorage.removeItem('catalogoScrollPos');
+        }
+    }
+});
+
