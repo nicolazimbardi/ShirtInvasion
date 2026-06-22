b8661933534f9b1579d794314d50a597ecc119f7
Nicola Zimbardi <n.zimbardi1@studenti.unisa.it>
2026-06-22 16:30:52 +0200

    modifica errore scroll js

--- ShirtInvasion/src/main/WebContent/scripts/script.js
+++ ShirtInvasion/src/main/WebContent/scripts/script.js
@@ -1,3 +1,29 @@
+// Funzione  per la modifica rapida 
+function modificaRapida(id, stagione, marca, taglia, prezzo, stock, descrizione) {
+    let nuovaStagione = prompt("Modifica Stagione:", stagione);
+    if (nuovaStagione == null) return;
+    let nuovaMarca = prompt("Modifica Marca:", marca);
+    if (nuovaMarca == null) return;
+    let nuovaTaglia = prompt("Modifica Taglia:", taglia);
+    if (nuovaTaglia == null) return;
+    let nuovoPrezzo = prompt("Modifica Prezzo (€):", prezzo);
+    if (nuovoPrezzo == null) return;
+    let nuovoStock = prompt("Modifica Stock (Quantità):", stock);
+    if (nuovoStock == null) return;
+    let nuovaDesc = prompt("Modifica Descrizione:", descrizione);
+    if (nuovaDesc == null) return;
+
+    document.getElementById('hidStagione' + id).value = nuovaStagione;
+    document.getElementById('hidMarca' + id).value = nuovaMarca;
+    document.getElementById('hidTaglia' + id).value = nuovaTaglia;
+    document.getElementById('hidPrezzo' + id).value = nuovoPrezzo;
+    document.getElementById('hidStock' + id).value = nuovoStock;
+    document.getElementById('hidDesc' + id).value = nuovaDesc;
+
+    document.getElementById('formModifica' + id).submit();
+}
+
+// Gestione Anteprima Immagine (Pannello Admin)
 document.addEventListener('DOMContentLoaded', function() {
     const selectImmagine = document.getElementById('scelta-immagine');
     
@@ -19,7 +45,8 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
-// Gestione  dello scroll al click di aggiungi al carrello
+
+// Gestione dello scroll al click di aggiungi al carrello 
 document.addEventListener('DOMContentLoaded', function() {
     const pulsantiCarrello = document.querySelectorAll('.btn-add-cart');
     
@@ -37,4 +64,3 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
-
